ef7ba8d3c41c02e4eaa009ca09e7e26724390a64
Gabriel Stellini <gs703@live.mdx.ac.uk>
2018-01-14 12:34:34 +0100

    Add NN algorithm

--- src/Controller/Algorithm.java
+++ src/Controller/Algorithm.java
@@ -3,14 +3,20 @@ package Controller;
 import Model.ImageData;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 
-public abstract class Algorithm{
+public abstract class Algorithm extends Thread{
 
-    abstract void run();
+    //non thread safe method
+    abstract public void run(List<ImageData> list, ImageData imageToCompare);
 
     public static int getRandomIndex(LinkedList<ImageData> data) {
-        int rnd = new Random().nextInt(data.size());
-        return rnd;
+        return new Random().nextInt(data.size());
     }
+
+
+    public abstract int getBestIndex();
+    public abstract double getBestDistance();
+    public abstract int getNumber();
 }
---
+++ src/Controller/NearestNeighbor.java
@@ -0,0 +1,81 @@
+package Controller;
+
+import Model.ImageData;
+
+import java.util.List;
+
+public class NearestNeighbor extends Algorithm {
+
+    private int bestIndex = -1;
+    private double bestDistance = -1;
+    private int number = -1;
+    private List<ImageData> list;
+    private ImageData imageToCompare;
+
+    public NearestNeighbor(){
+
+    }
+
+    public NearestNeighbor(List<ImageData> list, ImageData imageToCompare){
+        this.list = list;
+        this.imageToCompare = imageToCompare;
+    }
+
+    //NOT THREAD SAFE
+    public void run(List<ImageData> list, ImageData imageToCompare){
+        this.list = list;
+        this.imageToCompare = imageToCompare;
+        run();
+    }
+
+    @Override
+    public void run() {
+        //set an initial record
+        bestIndex = 0;
+        bestDistance = getDistance(list.get(0), imageToCompare);
+        number = list.get(getBestIndex()).getNumber();
+
+
+        double currentDistance;
+
+        for (int i = 1; i < list.size(); i++){
+            currentDistance = getDistance(list.get(i), imageToCompare);
+            if(currentDistance < getBestDistance()){
+                bestIndex = i;
+                bestDistance = currentDistance;
+                number = list.get(getBestIndex()).getNumber();
+            }
+        }
+    }
+
+    private double getDistance(ImageData imageData1, ImageData imageData2){
+        int[] image1BlackPixels = imageData1.getNumOfBlackPixels();
+        int[] image2BlackPixels = imageData2.getNumOfBlackPixels();
+
+        int distanceSquared = 0;
+
+        int deltaPixels;
+        for (int i = 0; i < image1BlackPixels.length; i++){
+            deltaPixels = image1BlackPixels[i] - image2BlackPixels[i];
+            distanceSquared += Math.pow(deltaPixels, 2);
+        }
+
+        return Math.sqrt(distanceSquared);
+    }
+
+    public int getBestIndex() {
+        return bestIndex;
+    }
+
+    public double getBestDistance() {
+        return bestDistance;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public boolean isCorrect(){
+        return number == imageToCompare.getNumber();
+    }
+}
