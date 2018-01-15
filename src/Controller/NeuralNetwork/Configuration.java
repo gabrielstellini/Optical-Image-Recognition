a9b5473e934bade3a672e5e099d8568eab3f5455
Gabriel Stellini <gs703@live.mdx.ac.uk>
2018-01-15 13:34:02 +0100

    Update maxEpochs

--- src/Controller/NeuralNetwork/Configuration.java
+++ src/Controller/NeuralNetwork/Configuration.java
@@ -1,19 +1,13 @@
 package Controller.NeuralNetwork;
 
-//ANN
-
 public class Configuration {
     final static int hiddenNeurons = 24;
     final static int hiddenLayers = 3;
     final static float teachingStep = 0.04f;
-    final static float leastMeanSquareError = 0.01f;
-    //multiplied by teachingStep so as to get a value closer to what we want
-    final static float momentum = 0.4f;
-    final static int maxEpochs = 40;
+    final static float leastMeanSquareError = 0.01f; //training will stop when error is lower than this
+    final static float momentum = 0.4f; //multiplied by teachingStep so as to get a value closer to what we want
+    final static int maxEpochs = 5000; //training will stop when epochs is higher than this
 
-    //do not change
     final static int outputN = 10; //total possible outputs (0 to 9)
     final static int inputN = 64; //total inputs into the network (64 data points for each image)
-
-
 }
